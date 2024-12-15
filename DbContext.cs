2e69fd3bd76ec7b2c6fc9a63425b8de371e32654
S00235076 <S00235076@atu.ie>
2024-12-15 00:11:20 +0000

    Add project files.

---
+++ DbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pets.data.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Pets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vet> Pets { get; set; }
+        public DbSet<VetVisit> VetVisits { get; set; }
+        public DbSet<Clinic> Clinics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Pets)
+                .WithOne(p => p.Owner)
+                .HasForeignKey(p => p.UserId);
+
+            modelBuilder.Entity<Vet>()
+                .HasMany(p => p.VetVisits)
+                .WithOne(v => v.Pet)
+                .HasForeignKey(v => v.PetId);
+
+            modelBuilder.Entity<Clinic>()
+                .HasMany(c => c.VetVisits)
+                .WithOne(v => v.Pet);
+        }
+    }
+}
---
+++ Vet.cs
@@ -0,0 +1,34 @@
+﻿namespace Pets.data.Models
+{
+    public class User : IdentityUser // Extend IdentityUser
+    {
+        public string FullName { get; set; }
+        public ICollection<Vet> Pets { get; set; } = new List<Vet>();
+    }
+
+    public class Vet
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; } 
+        public int UserId { get; set; }
+        public User Owner { get; set; }
+        public ICollection<VetVisit> VetVisits { get; set; } = new List<VetVisit>();
+    }
+
+    public class VetVisit
+    {
+        public int VetVisitId { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+        public int PetId { get; set; }
+        public Vet Pet { get; set; }
+    }
+
+    public class Clinic
+    {
+        public int ClinicId { get; set; }
+        public string Name { get; set; }
+        public ICollection<VetVisit> VetVisits { get; set; } = new List<VetVisit>();
+    }
+}
